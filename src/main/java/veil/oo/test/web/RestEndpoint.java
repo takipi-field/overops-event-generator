7809999d14809d27dd067a2da6efde39fc24037d
Chris Caspanello <ccaspanello@users.noreply.github.com>
2018-03-06 11:30:37 -0500

    trying to get http errors to occur

--- src/main/java/veil/oo/test/web/RestEndpoint.java
+++ src/main/java/veil/oo/test/web/RestEndpoint.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,7 +15,6 @@ public class RestEndpoint {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping("/throw500")
-    @ResponseBody
     public String throw500(HttpServletRequest request, HttpServletResponse response) {
 
         try {
@@ -29,7 +27,6 @@ public class RestEndpoint {
     }
 
     @GetMapping("/throw404")
-    @ResponseBody
     public String throw404(HttpServletRequest request, HttpServletResponse response) {
 
         try {
