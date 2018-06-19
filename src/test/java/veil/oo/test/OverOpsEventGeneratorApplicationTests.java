27ffce1428ba0e37f2925ff33e700eef88510b44
Tim Veil <tjveil@gmail.com>
2018-06-19 11:51:28 -0400

    adding contexts and metrics

--- src/test/java/veil/oo/test/OverOpsEventGeneratorApplicationTests.java
+++ src/test/java/veil/oo/test/OverOpsEventGeneratorApplicationTests.java
@@ -3,7 +3,6 @@ package veil.oo.test;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,8 +11,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @ActiveProfiles("test")
 public class OverOpsEventGeneratorApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
