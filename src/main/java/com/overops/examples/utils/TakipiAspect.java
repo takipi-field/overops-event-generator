4d87ccb67ae38f2488fc4784ce14d4a0bbd1d5c8
Chris Caspanello <ccaspanello@users.noreply.github.com>
2018-09-05 11:46:07 -0400

    adding some logging around SDK

--- src/main/java/com/overops/examples/utils/TakipiAspect.java
+++ src/main/java/com/overops/examples/utils/TakipiAspect.java
@@ -9,6 +9,8 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StopWatch;
@@ -16,6 +18,9 @@ import org.springframework.util.StopWatch;
 @Aspect
 @Configuration
 public class TakipiAspect {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
 
     private final Takipi takipi;
 
@@ -27,7 +32,11 @@ public class TakipiAspect {
     @Before("@annotation(com.overops.examples.utils.TakipiInvocationCounter)")
     public void count(JoinPoint joinPoint) throws Throwable {
 
-        TakipiContext context = takipi.contexts().createContext(joinPoint.getSignature().getDeclaringType());
+        Class declaringType = joinPoint.getSignature().getDeclaringType();
+
+        log.info("creating 'count' context for class : {}", declaringType);
+
+        TakipiContext context = takipi.contexts().createContext(declaringType);
 
         TakipiCountMetric countMetric = takipi.metrics().createCountMetric("COUNT_" + joinPoint.getSignature().getName());
 
@@ -38,7 +47,11 @@ public class TakipiAspect {
     @Around("@annotation(com.overops.examples.utils.TakipiAverageExecutionTime)")
     public Object average(ProceedingJoinPoint joinPoint) throws Throwable {
 
-        TakipiContext context = takipi.contexts().createContext(joinPoint.getSignature().getDeclaringType());
+        Class declaringType = joinPoint.getSignature().getDeclaringType();
+
+        log.info("creating 'average' context for class : {}", declaringType);
+
+        TakipiContext context = takipi.contexts().createContext(declaringType);
 
         String metricName = "AVERAGE_" + joinPoint.getSignature().getName();
 
