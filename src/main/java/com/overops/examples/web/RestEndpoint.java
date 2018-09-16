2cdf08711ff778e381ed1b1b232e1e0ed89c73d3
Tim Veil <tjveil@gmail.com>
2018-09-16 15:10:48 -0400

    more cleanup

--- src/main/java/com/overops/examples/web/RestEndpoint.java
+++ src/main/java/com/overops/examples/web/RestEndpoint.java
@@ -3,13 +3,12 @@ package com.overops.examples.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class RestEndpoint {
@@ -18,18 +17,16 @@ public class RestEndpoint {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-    @RequestMapping(path = "/throw500", method = RequestMethod.GET)
-    public ResponseEntity throw500(HttpServletRequest request, HttpServletResponse response) {
+    @RequestMapping(path = "/throw500", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> throw500(@RequestParam(value = GENERATE_EVENT, required = false, defaultValue = "false") boolean generateEvent) {
 
-        String generateEvent = request.getParameter(GENERATE_EVENT);
+        if (generateEvent) {
+            log.debug("setting HttpStatus = {}", HttpStatus.INTERNAL_SERVER_ERROR);
 
-        if (generateEvent != null && generateEvent.equalsIgnoreCase(Boolean.TRUE.toString())) {
-            log.debug("going to call response.sendError() with {}", HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-
-            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>("error generated", HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
-        return new ResponseEntity(HttpStatus.OK);
+        return new ResponseEntity<>("ok", HttpStatus.OK);
     }
 
 }
